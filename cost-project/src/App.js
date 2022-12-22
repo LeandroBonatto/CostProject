97a46a6138278f1e5a16f94ea29c283b1da4e237
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-22 00:52:00 -0500

    Update App.js

--- cost-project/src/App.js
+++ cost-project/src/App.js
@@ -5,11 +5,15 @@ function App() {
 
   const newName = name.toUpperCase()
 
+  function sum(a, b) {
+    return a + b
+  }
+
   return (
     <div className="App">
       <h2>Changing the JSX</h2>
       <p>Hello, {newName}</p>
-      <p>Sum: {2 + 2}</p>
+      <p>Sum: {sum(1, 2)}</p>
     </div>
   );
 }
