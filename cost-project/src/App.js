08fd31cd2ae33f7d3d5575a3238689f498432bd8
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-22 23:41:55 -0500

    Update App.js

--- cost-project/src/App.js
+++ cost-project/src/App.js
@@ -2,22 +2,9 @@ import './App.css'
 import HelloWorld from './components/HelloWorld'
 
 function App() {
-  const name = 'Leo'
-
-  const newName = name.toUpperCase()
-
-  function sum(a, b) {
-    return a + b
-  }
-
-  const url = 'https://via.placeholder.com/150'
-
   return (
     <div className="App">
-      <h2>Changing the JSX</h2>
-      <p>Hello, {newName}</p>
-      <p>Sum: {sum(1, 2)}</p>
-      <img src={url} alt="My picture" />
+ 
       <HelloWorld/>
     </div>
   );
