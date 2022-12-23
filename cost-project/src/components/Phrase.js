7aa4f3a57752165f0f9f84bd2c8f808de19707f7
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-23 01:24:28 -0500

    Update Phrase.js

--- cost-project/src/components/Phrase.js
+++ cost-project/src/components/Phrase.js
@@ -3,7 +3,7 @@ import styles from './Phrase.module.css'
 function Phrase() {
     return(
         <div  className={styles.fraseContainer}>
-            <p>This is Phrase component</p>
+            <p className={styles.PhraseContent}>This is Phrase component</p>
         </div>
     )
 
