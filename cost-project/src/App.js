ba61e682040249179d696a07771e0cfdf8bf5dae
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-23 00:34:17 -0500

    Update App.js

--- cost-project/src/App.js
+++ cost-project/src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import HelloWorld from './components/HelloWorld'
 import SayMyName from './components/SayMyName'
+import Person from './components/Person'
 
 function App() {
 
@@ -12,8 +13,9 @@ function App() {
       <SayMyName name="Matt"/>
       <SayMyName name="John"/>
       <SayMyName name={name} />
+      <Person name="Rod" age="28" carrer="Programmer" pic="https://via.placeholder.com/150" />
     </div>
-  );
+  )
 }
 
 export default App
