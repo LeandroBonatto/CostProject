d3d1882a05a59e8f54c16c0fdda09d15b6a921db
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-23 01:22:18 -0500

    Update App.js

--- cost-project/src/App.js
+++ cost-project/src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import HelloWorld from './components/HelloWorld'
 import SayMyName from './components/SayMyName'
 import Person from './components/Person'
+import Phrase from './components/Phrase'
 
 function App() {
 
@@ -9,7 +10,9 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Testando CSS</h1>
+      <h1>Testing CSS</h1>
+      <Phrase />
+      <Phrase />
       <SayMyName name="Matt"/>
       <SayMyName name="John"/>
       <SayMyName name={name} />
