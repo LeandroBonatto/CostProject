4417fd8d3f0d012cdd849622306c0da31780ecd6
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-22 23:48:05 -0500

    Update App.js

--- cost-project/src/App.js
+++ cost-project/src/App.js
@@ -1,11 +1,12 @@
 import './App.css'
 import HelloWorld from './components/HelloWorld'
+import SayMyName from './components/SayMyName'
 
 function App() {
   return (
     <div className="App">
- 
       <HelloWorld/>
+      <SayMyName/>
     </div>
   );
 }
