cc86e359372b715fc830674d590e4ebe0d976dba
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-23 00:27:49 -0500

    Update Person.js

--- cost-project/src/components/Person.js
+++ cost-project/src/components/Person.js
@@ -1,9 +1,12 @@
-function Person() {
+function Person(props) {
 
     return (
         <div>
-            <img src={props.pic} />
-            </img>
+            <img src={props.pic} alt={props.name} />
+            <h2>Name: {props.name}</h2>
+            <p>Age: {props.age}</p>
+            <p>Carrer: {props.carrer}</p>
+            
         </div>
     )
 }
