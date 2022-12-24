517fea02458cde4aa5d1a5e10bf4b7eba541a3a9
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-24 16:40:48 -0500

    Create Form.js

---
+++ cost-project/src/components/Form.js
@@ -0,0 +1,18 @@
+function Form() {
+
+    function
+
+    return (
+        <div>
+            <h1>Fill up:</h1>
+            <form onSubmit={Register}>
+                <div>
+                    <input type="text" placeholder="Write your name" ></input>
+                </div>
+                <div>
+                    <input type="submit" value="registration"></input>
+                </div>
+            </form>
+        </div>
+    )
+}
