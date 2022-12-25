8333687f7bd63d4a4d97be1a061db9fad74ba42d
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-25 18:08:15 -0500

    Update Condicional.js

--- cost-project/src/components/Condicional.js
+++ cost-project/src/components/Condicional.js
@@ -1,10 +1,19 @@
 import { useState } from "react"
 
 function Condicional () {
+
+    function sendEmail(e) {
+        e.preventDefault()
+    }
+
     return (
         <div>
-            <h2>Log in your email:</h2>
-            <form></form>
+            <h2>Sign in your email:</h2>
+            <form>
+                <input type="email" placeholder="Write your email" />
+                <button onClick={sendEmail}>Send email</button>
+            </form>
+
         </div>
 
     )
