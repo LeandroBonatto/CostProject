d137807111bb611bf07b04ef98ca779ad42d36d3
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-26 01:13:47 -0500

    Update Salutation.js

--- cost-project/src/components/Salutation.js
+++ cost-project/src/components/Salutation.js
@@ -1,15 +1,9 @@
 function Salutation({name}) {
     function generateSalutation(anyName) {
-    return 'Hello, ${anyName}, everything ok?'
+        return `Hello, ${anyName}, everything ok?`
     }
 
-
-
-    return (
-    <>
-        <p>{generateSalutation(name)}</p>
-    </>
-    )
+    return <>{name && <p>{generateSalutation(name)}</p>}</>
 }
 
 export default Salutation
