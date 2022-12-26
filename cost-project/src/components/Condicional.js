bfb15255cbc9c0f3865a6f1cb29aed1e0582cc87
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-25 19:24:14 -0500

    Update Condicional.js

--- cost-project/src/components/Condicional.js
+++ cost-project/src/components/Condicional.js
@@ -2,16 +2,24 @@ import { useState } from "react"
 
 function Condicional () {
 
+    const [email, setEmail] = useState()
+
+
     function sendEmail(e) {
         e.preventDefault()
+        console.log('Testing')
     }
 
     return (
         <div>
             <h2>Sign in your email:</h2>
             <form>
-                <input type="email" placeholder="Write your email" />
-                <button onClick={sendEmail}>Send email</button>
+                <input type="email" placeholder="Write your email" 
+                onChange={(e) => setEmail(e.target.value)} 
+                />
+                <button type="submit" onClick={sendEmail}>
+                    Send email
+                </button>
             </form>
 
         </div>
