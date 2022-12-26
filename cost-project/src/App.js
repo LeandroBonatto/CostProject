5fe3acf492397ca5ec10bb62a56faf8699bffc48
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-26 01:36:55 -0500

    npminstallreactrouterdom

--- cost-project/src/App.js
+++ cost-project/src/App.js
@@ -1,19 +1,10 @@
-import { useState } from 'react'
-import './App.css'
-import Salutation from './components/Salutation'
-import YourName from './components/YourName'
 
 
 function App() {
-  const [name, setName] = useState()
 
 
   return (
-    <div className="App">
-      <h1>State Lift</h1>
-      <YourName setName={setName}  />
-      <Salutation name={name} />
-    </div>
+    
   )
 }
 
