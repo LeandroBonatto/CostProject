6a755a225bc0a1fbb6e2ece6bc176dd8d38e10f6
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-26 21:21:57 -0500

    package

--- cost-project/src/components/layout/Footer.js
+++ cost-project/src/components/layout/Footer.js
@@ -1,7 +1,24 @@
-import {} from 'react-icon/fa'
+import { FaFacebook, FaInstagram } from 'react-icon/fa'
+
+import styles from './Footer.module.css'
 
 function Footer() {
-    return <Footer>Footer</Footer>
+    return (
+        <Footer>
+            <ul className={styles.social_list}>
+                <li>
+                    <FaFacebook  />
+                </li>
+                <li>
+                    <FaInstagram />
+                </li>
+                <li>
+                    <FaLinkedin />
+                </li>
+            </ul>
+            <p>Footer part</p>
+        </Footer>
+    )
 }
 
 export default Footer
